cf48414a42688291d380f2f1f928d1f74a21ddbf
ZION959 <ziontran@gmail.com>
2015-02-15 18:45:51 -0700

    trlte: Enable apps2sd (temasek)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,9 @@ TARGET_CPU_VARIANT := krait
 # L1/L2 cache size parameters by @JustArchi
 BOARD_GLOBAL_CFLAGS := --param l1-cache-size=16 --param l1-cache-line-size=16 --param l2-cache-size=2048
 
+# External apps on SD
+TARGET_EXTERNAL_APPS = sdcard1
+
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
