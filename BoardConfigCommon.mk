527bf149114bc2b8b7bee8f83796307750780ea3
ZION959 <ziontran@gmail.com>
2015-06-02 22:33:16 -0700

    Turn art optimization to everything
    credit @fusionjack
    Credits to @Sykopompos (Chroma-AOSP)
    
    By default, art optimization is set to speed : speed - compiles most methods and maximizes runtime performance
    but we are changing to everything here : everything - compiles almost everything, excluding class
    initializers and some rare methods that are too large to be represented by the compiler’s internal representation.
    
    ART is a different beast then dalvik and by creating oat files with more in them we are speeding up loading
    this creates a trade off of a minute or 2 optimizing apps at boot for quicker and steadier performance
    
    Make sure users wipe their cache after first flashing like this to force creation of new oat files
    
    More infos about this optimization:
    https://plus.google.com/+SykoPompos/posts/eDUgU11ZVxQ
    https://plus.google.com/+SykoPompos/posts/ag3fiRZUArD
    
    Change-Id: I9ecd75ca1302d58132325f044c83baa2f5c6cea5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,3 +180,8 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# ART optimization
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.image-dex2oat-filter=everything \
+    dalvik.vm.dex2oat-filter=everything
+
